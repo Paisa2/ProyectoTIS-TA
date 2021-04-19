f6d101790cac214517c9c1d240c42768036b1c6e
cruzito <ru.cruz.gonza10@gmail.com>
2021-04-19 00:58:11 -0400

    validacion primera parte

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\phpLoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
