0bfdbf3b6b161f87aba0dea32cfbebfc9e845aa3
Richard <nightwing_96@hotmail.com>
2021-04-27 02:00:35 -0400

    adicion de estilos en crear y visualizar roles

--- resources/views/roles/crearRol.blade.php
+++ resources/views/roles/crearRol.blade.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
-<body>
+@extends('base')
 
+@section('head')
+<link rel="stylesheet" href="{{ asset('css/forms.css') }}">
+@endsection
+
+@section('main')
 <!-- codigo importante -->
 
 <div style="width:90%; margin:24px auto;">
@@ -59,6 +55,5 @@
 </div>
 
 <!-- fin codigo importante -->
-  
-</body>
-</html>
+
+@endsection
--- resources/views/roles/visualizarRoles.blade.php
+++ resources/views/roles/visualizarRoles.blade.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-</head>
-<body>
+@extends('base')
+
+@section('head')
+<link rel="stylesheet" href="{{ asset('css/tables.css') }}">
+@endsection
+
+@section('main')
   
 <!-- codigo importante -->
 
-<div style="width:90%; margin:24px auto;">
+<div style="width:90%; margin:24px auto;" class="container-table">
 
   <h1>Roles Registrados</h1>
   <br>
@@ -43,5 +40,4 @@
 
 <!-- fin codigo importante -->
 
-</body>
-</html>
+@endsection
