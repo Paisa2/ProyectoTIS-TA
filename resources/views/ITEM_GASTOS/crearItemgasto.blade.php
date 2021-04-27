e55f91bb0e9c814d8a992fdbdefd1c5a37e38c23
ALDAR-G7 <ronnyald007@gmail.com>
2021-04-27 13:50:44 -0400

    Interfaz del Formulario Registro Item de Gasto

--- resources/views/ITEM_GASTOS/crearItemgasto.blade.php
+++ resources/views/ITEM_GASTOS/crearItemgasto.blade.php
@@ -1,21 +1,16 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Items de Gasto</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  </head>
-  <body>
-    <div class="container my-4">
+@extends('base')
+@section('head')
+<link rel="stylesheet" href="{{ asset('css/forms.css') }}">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+@endsection
+@section('main')
+<div class="container my-4">
+       
+       <form method="post" action="{{ route('itemsgastos') }}">
+       {{csrf_field()}}
        <h1 class="display-4">
         Registro de item de gasto
        </h1>
-       <form method="post" action="{{ route('itemsgastos') }}">
-       {{csrf_field()}}
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Tipo:</label>
     <br>
@@ -32,7 +27,7 @@
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Nombre:</label>
-    <input type="text" class="form-control" id="exampleInputPassword1" name="nombre_item">
+    <input type="text" class="form-control" id="exampleInputPassword1" name="nombre_item" value="{{ old('nombre_item') }}">
     <div class="br"> </div>
     @foreach($errors->get('nombre_item') as $message)
     <div class="alert alert-danger">{{$message}}</div>
@@ -54,6 +49,29 @@
   <button type="submit" class="btn btn-primary">REGISTRAR</button>
 </form>
     </div>
+@endsection
+@section('scripts')
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <script>
+    $('input[name="tipo_item"]').on('change',function()
+    {
+      $('select[name="item_id"]').attr('disabled',this.value!="Especifico")
+    });
+    </script>
+@endsection
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Items de Gasto</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  </head>
+  <body>
+    
 
     <!-- Optional JavaScript; choose one of the two! -->
 
