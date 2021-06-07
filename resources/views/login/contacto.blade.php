d099cc8f31f6c4c31af7861c0209929f343d78ec
cruzito <ru.cruz.gonza10@gmail.com>
2021-06-06 20:34:22 -0400

    segunta parte

--- resources/views/login/contacto.blade.php
+++ resources/views/login/contacto.blade.php
@@ -1,18 +1,105 @@
-@extends('base')
-@section('title')
-AtorizaciónPresupuesto
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/217bbe5a21.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link  href="{{ asset('css/forms.css') }}"  rel="stylesheet">
+    <link  href="{{ asset('css/contacto.css') }}"  rel="stylesheet">
 
-@section('head')
-<title>AtorizaciónPresupuesto</title>
-<link rel="stylesheet" href="{{ asset('css/forms.css') }}">
-<link rel="stylesheet" href="{{ asset('css/AutorizaciónSolicitud.css') }}">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title>contacto</title>
+</head>
+<body>
+    <nav class= "nav">
+         <ul>
+           <li><a href="/contacto">Información</a></li>
+           <li><a href="/soporte">Soporte</a></li>
+           <li><a href="/login">Inicio Sesión</a></li>
+         </ul> 
+    </nav>
+       <div class="container">
+          <div class='row'>
+             <div class='col-4'>
+             <br>
+            
+                
+                 
+               <h2>Contactos</h2>
+                   <h3>Relaciones Publicas</h3>
+                   
+                  <div style="display: flex; padding: 0px 15px 0px 15px;">
+                      
+                   <div class="iconCheck"><i class="fas fa-envelope"></i></div> 
+                   <label class='alin'>rr.pp@umss.edu.bo</label>
+                  </div> 
 
-@endsection
+                  <div style="display: flex; padding: 0px 15px 0px 15px;">
+                      
+                  <i class="fas fa-phone-alt"></i>
+                   <label class='alin'>(+591)4 4251515; (+591)4 4525161</label>
+                  </div> 
 
+                  <div style="display: flex; padding: 0px 15px 0px 15px;">
+                      
+                  <i class="fas fa-fax"></i>
+                   <label class='alin'>(+591)4 4525161</label>
+                  </div> 
+                  <h3>Secretaria General</h3>
+                  
+                  <div style="display: flex;  padding: 0px 15px 0px 15px;">
+                      
+                   <div class="iconCheck"><i class="fas fa-envelope"></i></div> 
+                   <label class='alin'>s.gral@gmail.umss.edu.bo</label>
+                  </div> 
 
+                  <div style="display: flex; padding: 0px 15px 0px 15px;">
+                      
+                  <i class="fas fa-phone-alt"></i>
+                   <label class='alin'> (+591)4 4525161</label>
+                  </div> 
 
-@section('scripts')
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-@endsection
+                  <div style="display: flex;  padding: 0px 15px 0px 15px;">
+                      
+                  <i class="fas fa-fax"></i>
+                   <label class='alin'>(+591)4 4524772</label>
+                  </div> 
+                  <h3>Información General</h3>
+
+                  <div style="display: flex; padding: 0px 15px 0px 15px;">
+                      
+                      <div class="iconCheck"><i class="fas fa-envelope"></i></div> 
+                      <label class='alin'>información@umss.edu.bo</label>
+                     </div> 
+                     <div class='imagen'>
+                 <img src="/imagenes/cotysoft1.png"> 
+                 </div>
+                   
+             </div>
+             <div class='col-4'>
+                 <br>
+                 <br>
+                 <br>
+             <a class='image'>
+                 <img src="/imagenes/coty.png"> 
+                 </a>
+             </div>
+             <div class='col-4'>
+             <br>
+             <br>
+             <br>
+             <h2>Sistema de Información</h2>
+                 <div class='bt_bb_headline_subheadline'>
+                     "Queremos asesorarle de principio a fin ayudandole a cotizar de manera trasparante e instantanea para tu institución"
+                 </div>
+                 <br>
+                 <h2>Soporte</h2>
+              </div>
+
+           </div>
+        </div>
+</body>
+</html>
