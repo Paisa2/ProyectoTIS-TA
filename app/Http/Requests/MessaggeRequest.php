8c2fd425fbe0b8890e0e9e4d1ce4fc7d531e16f1
christian <christianrq77@gmail.com>
2021-06-19 13:44:50 -0400

    arreglos registro de empresa

--- app/Http/Requests/MessaggeRequest.php
+++ app/Http/Requests/MessaggeRequest.php
@@ -36,13 +36,13 @@ class MessaggeRequest extends FormRequest
     }
     public function messages(){
         return[
-            'nombre_unidad.required' => 'El campo "Nombre" debe ser llenado',
-            'nombre_unidad.min' => 'El campo "Nombre" debe tener mas de 2 caracteres',
-            'nombre_unidad.max' => 'El campo "Nombre" no debe tener mas de 255 caracteres',
-            'nombre_unidad.regex' => 'El campo "Nombre" solo permite caracteres alfabeticos',
+            'nombre_unidad.required' => 'El campo Nombre es requerido',
+            'nombre_unidad.min' => 'El campo Nombre debe tener mas de 2 caracteres',
+            'nombre_unidad.max' => 'El campo Nombre no debe tener mas de 255 caracteres',
+            'nombre_unidad.regex' => 'El campo Nombre solo permite caracteres alfabeticos',
             'nombre_unidad.unique' => 'La unidad ya ha sido registrada',
             'tipo_unidad.required' => 'El campo tipo es requerido',
-            'unidad_id.required' => 'El campo "Pertenece A" es requerido'
+            'unidad_id.required' => 'El campo Pertenece A es requerido'
         ];
     }
 }
